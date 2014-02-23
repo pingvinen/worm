6fc15bfcca8b0e81ba343ab3c5344c4865d1958f
Patrick <pingvinen@isflagen.dk>
2014-02-23 17:33:40 +0100

    Added tests for PocoFieldCollection.GetPublicFields()

--- src/wormlib-tests/Parsing/Internals/PocoFieldCollectionTests.cs
+++ src/wormlib-tests/Parsing/Internals/PocoFieldCollectionTests.cs
@@ -3,6 +3,8 @@ using Moq;
 using NUnit.Framework;
 using Worm.Parsing.Internals;
 using Worm.CodeGeneration.Internals;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Wormlibtests.Parsing.Internals
 {
@@ -24,6 +26,7 @@ namespace Wormlibtests.Parsing.Internals
 			this.collection = new PocoFieldCollection();
 		}
 
+		#region GetPrimaryKeyField
 		[Test]
 		public void GetPrimaryKeyField_emptyFieldSet()
 		{
@@ -63,5 +66,68 @@ namespace Wormlibtests.Parsing.Internals
 
 			Assert.AreEqual(this.field2.Object, this.collection.GetPrimaryKeyField());
 		}
+		#endregion
+
+		#region GetPublicFields
+		[Test]
+		public void GetPublicFields_emptyFieldSet()
+		{
+			IEnumerable<PocoField> actual = this.collection.GetPublicFields();
+			Assert.IsEmpty(actual);
+		}
+
+		[Test]
+		public void GetPublicFields_returnsPublicFieldsOnly()
+		{
+			this.field1.SetupGet(xx => xx.AccessModifier).Returns(AccessModifier.Public);
+			this.field1.SetupGet(xx => xx.HasSetter).Returns(true);
+			this.field1.SetupGet(xx => xx.IsPrimaryKey).Returns(false);
+
+			this.field2.SetupGet(xx => xx.AccessModifier).Returns(AccessModifier.Private);
+			this.field2.SetupGet(xx => xx.HasSetter).Returns(true);
+			this.field2.SetupGet(xx => xx.IsPrimaryKey).Returns(false);
+
+			this.field3.SetupGet(xx => xx.AccessModifier).Returns(AccessModifier.Public);
+			this.field3.SetupGet(xx => xx.HasSetter).Returns(true);
+			this.field3.SetupGet(xx => xx.IsPrimaryKey).Returns(false);
+
+			this.collection.Add(this.field1.Object);
+			this.collection.Add(this.field2.Object);
+			this.collection.Add(this.field3.Object);
+
+			IList<PocoField> actual = this.collection.GetPublicFields().ToList();
+
+			Assert.AreEqual(this.field1.Object, actual[0]);
+			Assert.AreEqual(this.field3.Object, actual[1]);
+		}
+
+		[Test]
+		public void GetPublicFields_returnsPublicFieldsOnly_butNotPrimaryKeyFields()
+		{
+			this.field1.SetupGet(xx => xx.AccessModifier).Returns(AccessModifier.Public);
+			this.field1.SetupGet(xx => xx.HasSetter).Returns(true);
+			this.field1.SetupGet(xx => xx.IsPrimaryKey).Returns(true);
+
+			this.collection.Add(this.field1.Object);
+
+			IList<PocoField> actual = this.collection.GetPublicFields().ToList();
+
+			Assert.IsEmpty(actual);
+		}
+
+		[Test]
+		public void GetPublicFields_returnsPublicFieldsOnly_onlyIfFieldHasASetter()
+		{
+			this.field1.SetupGet(xx => xx.AccessModifier).Returns(AccessModifier.Public);
+			this.field1.SetupGet(xx => xx.HasSetter).Returns(false);
+			this.field1.SetupGet(xx => xx.IsPrimaryKey).Returns(false);
+
+			this.collection.Add(this.field1.Object);
+
+			IList<PocoField> actual = this.collection.GetPublicFields().ToList();
+
+			Assert.IsEmpty(actual);
+		}
+		#endregion
 	}
 }
